ffb506abcccfe67efa55697f360544f83f353177
DenilsonMelo <denilsonmlo42@gmail.com>
2023-05-17 11:09:28 -0300

    added categories repository and controller

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactController = require('./app/Controllers/ContactController');
+const CategoryController = require('./app/Controllers/CategoryController');
 
 const router = Router();
 
@@ -9,5 +10,11 @@ router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
+router.delete('/categories/:id', CategoryController.delete);
+router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
